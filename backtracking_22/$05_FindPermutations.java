4a6833fb204e736ed00e2701e75889714f70b042
yogesh chaudhary <yogeshchaudhary815>
2026-08-14 00:42:55 +0530

    Add implementation of Find Permutations in $05_FindPermutations.java

---
+++ backtracking_22/$05_FindPermutations.java
@@ -0,0 +1,24 @@
+package backtracking_22;
+
+public class $05_FindPermutations {
+    
+    public static void findPermutation(String str , String ans){
+
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i=0; i<str.length(); i++){
+             char curr = str.charAt(i);
+            String newStr = str.substring(0,i) + str.substring(i+1);
+            findPermutation(newStr, ans+curr);
+
+        }
+    }
+
+    public static void main(String[] args){
+        String str = "abc";
+        findPermutation(str, "");
+    }
+}
